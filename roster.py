80f8156e09fac82cd0e1df9ca522bd891216071a
delaneybrod <delaneyb@unc.edu>
2024-11-26 11:06:35 -0500

    final commit_ten players

--- roster.py
+++ roster.py
@@ -2,13 +2,16 @@
 
 import pandas as pd 
 
-player = {"Last name": ["Bacot", "Davis","Cadeau"],
-          "First Name": ["Armandoo", "RJ", "Elliot"],
-          "height": [83, 72, 73],
-          "weight":[240, 180, 180]}
+player = {"Last name": ["Bacot", "Davis","Cadeau", "Claude", "Tyson", "Trimble","Powell", "Jackson", "Washington", "Hawkins", "Holbrook", "Lubin", "Withers", "Mayo Jr."],
+          "First Name": ["Armandoo", "RJ", "Elliot", "Ty", "Cade", "Seth", "Drake", "Ian", "Jalen", "Russell", "John", "Ven-Allen", "Jae'Lyn", "Dante"],
+          "height": [83, 72, 73, 73, 79, 75, 76, 74, 82, 71, 78, 78, 79, 71],
+          "weight":[240, 180, 180, 251, 200, 195, 195, 190, 235, 175, 230, 230, 220, 175]}
 data = pd.DataFrame(player)
+
+#bmi = weight in kg/height in meters squared
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+
+data["bmi"] = data["bmi"].round(2)
 print(data)
 
-roster = ["Bacot", "Davis", "Cadaeu"]
-for player in roster:
-    print (roster)
+data.to_csv("bmi.cvs")
